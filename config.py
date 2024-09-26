8ecdbe6de616211bddaa9e48311d3b8c9851c62f
Alexander Lozhkin <alexand3r.lozhkin@gmail.com>
2024-09-26 17:23:39 +0500

    fix workflow

--- config.py
+++ config.py
@@ -4,3 +4,6 @@ TOKEN = os.environ["GNOMOTRON_TELEGRAM_TOKEN"]
 CHAT_ID = os.environ["GNOMOTRON_TELEGRAM_CHAT_ID"]
 API_KEY = os.environ["GNOMOTRON_TELEGRAM_API_KEY"]
 SHOULD_DROP_MESSAGES = True
+
+if not TOKEN or not CHAT_ID or not API_KEY:
+    raise RuntimeError("TOKEN, CHAT_ID and API_KEY must be specified via the env")
