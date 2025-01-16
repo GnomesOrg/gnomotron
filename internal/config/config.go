fbf2fd5494b3e9fe40099299a08391f810fe7ac6
dstartsev <mityastar01@gmail.com>
2025-01-16 17:27:05 +0500

    typo fix

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,7 +7,6 @@ import (
 
 type Config struct {
 	TOKEN     string
-	CHAT_ID   string
 	APIKEY    string
 	MONGO_URI string
 	MONGO_DB  string
@@ -17,13 +16,12 @@ type Config struct {
 func LoadConfig() *Config {
 	cfg := Config{}
 	cfg.TOKEN = os.Getenv("GNOMOTRON_TELEGRAM_TOKEN")
-	cfg.CHAT_ID = os.Getenv("GNOMOTRON_TELEGRAM_CHAT_ID")
 	cfg.APIKEY = os.Getenv("GNOMOTRON_TELEGRAM_API_KEY")
 	cfg.MONGO_URI = os.Getenv("GNOMOTRON_MONGO_URI")
 	cfg.MONGO_DB = os.Getenv("GNOMOTRON_MONGO_DB")
 	cfg.BOT_NAME = os.Getenv("BOT_NAME")
 
-	if cfg.TOKEN == "" || cfg.CHAT_ID == "" || cfg.APIKEY == "" || cfg.MONGO_URI == "" || cfg.MONGO_DB == "" || cfg.BOT_NAME == "" {
+	if cfg.TOKEN == "" || cfg.APIKEY == "" || cfg.MONGO_URI == "" || cfg.MONGO_DB == "" || cfg.BOT_NAME == "" {
 		log.Panic("All config fields must be specified via the environment variables")
 	}
 
