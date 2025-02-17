898e2297b834ec1880fd41e464a1919043ce6a6e
dstartsev <mityastar01@gmail.com>
2025-02-17 15:58:00 +0500

    server

--- commands/commandHandler.go
+++ commands/commandHandler.go
@@ -10,19 +10,21 @@ import (
 )
 
 type CommandHandler struct {
+	l   *slog.Logger
 	bot *tgbotapi.BotAPI
 }
 
 func NewCommandHandler(l *slog.Logger, b *tgbotapi.BotAPI) *CommandHandler {
 	return &CommandHandler{
 		bot: b,
+		l:   l,
 	}
 }
 
 type SendMsgReq struct {
 	Msg     string `json:"msg"`
 	ChatId  int64  `json:"chatId"`
-	ReplyId *int    `json:"replyId"`
+	ReplyId *int   `json:"replyId"`
 }
 
 func (ch *CommandHandler) SendMsgCommand(w http.ResponseWriter, r *http.Request) {
@@ -34,19 +36,18 @@ func (ch *CommandHandler) SendMsgCommand(w http.ResponseWriter, r *http.Request)
 	res := &SendMsgReq{}
 	err := json.NewDecoder(r.Body).Decode(res)
 	if err != nil {
+		ch.l.Error("error on decoding request", slog.Any("error", err))
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	m := tgbotapi.MessageConfig{}
+	m := tgbotapi.NewMessage(res.ChatId, res.Msg)
 
-	if res.ReplyId == nil {
-		m = tgbotapi.NewMessage(res.ChatId, res.Msg)
-	} else {
-		m = tgbotapi.NewMessage(res.ChatId, res.Msg)
+	if res.ReplyId != nil {
 		m.ReplyToMessageID = *res.ReplyId
-		
 	}
+
+	ch.l.Info("sending message", slog.Any("msg", res.Msg), slog.Any("chatId", res.ChatId), slog.Any("replyId", res.ReplyId))
 	ch.bot.Send(m)
 	w.WriteHeader(http.StatusCreated)
 
